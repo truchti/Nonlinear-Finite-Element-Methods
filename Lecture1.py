74173e345552f3f251830fa513dff452ed1e5ca3
truchti <35013354+truchti@users.noreply.github.com>
2018-01-08 17:45:46 -0700

    Working on 3D node array

--- Lecture1.py
+++ Lecture1.py
@@ -1,4 +1,48 @@
 
 
-def node_coors(dim, ):
-    
+def node_coors(dim, *args ):
+    """ This function takes inputs of the dimension (dim) and then dim*2 of parameters in the order number of elements of dim 1, total length of dim 1, ele 2, len 2, ele 3, len 3"""
+    #sorts arguments in to correct variables
+    if dim == 1:
+        if len(args) == 2:
+            m = args[0]
+            M = args[1]
+        else:
+            print("Warning wrong number of parameters")
+        x = 0
+        stepi = M/m
+
+    elif dim == 2:
+        if len(args) == 4:
+            m = args[0]
+            M = args[1]
+            n = args[2]
+            N = args[3]
+        else:
+            print("Warning wrong number of parameters")
+
+        x = 0
+        y = 0
+        stepi = M/m
+        stepj = N/n
+
+    elif dim == 3:
+        if len(args) == 6:
+            m = args[0]
+            M = args[1]
+            n = args[2]
+            N = args[3]
+            q = args[4]
+            Q = args[5]
+        else:
+            print("Warning wrong number of parameters")
+        x = 0
+        y = 0
+        z = 0
+        stepi = M/m
+        stepj = N/n
+        stepk = Q/q
+    else:
+        print("Function not equipped for more than 3 dimensional problem")
+
+
